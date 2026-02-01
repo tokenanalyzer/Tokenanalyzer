be29b42fd2b8f4e56a22042295b0cbfb0d930e12
tokenanalyzer <tokenanalyzerhelp@gmail.com>
2026-02-01 06:37:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,5 @@ def home():
     return "TokenAnalyzer Live"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
