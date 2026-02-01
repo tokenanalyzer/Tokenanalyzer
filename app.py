53cc7074c8d8d642633c241d92989738670f81f1
tokenanalyzer <tokenanalyzerhelp@gmail.com>
2026-02-01 09:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, Response
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
-
-@app.route("/validation-key.txt")
-def validation():
-    return Response("c8a59a8e935f8abb56afb73e0d02b2", mimetype="text/plain")
 
 @app.route("/")
 def home():
     return "TokenAnalyzer Live"
+
+@app.route("/validation-key.txt")
+def validation():
+    return send_from_directory(".", "validation-key.txt")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
