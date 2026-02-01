2b7d9be1e1a058d6416cc476dc531243be553f6a
tokenanalyzer <tokenanalyzerhelp@gmail.com>
2026-02-01 08:10:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
+
 from flask import Flask, Response
 import os
 
 app = Flask(__name__)
 
+VALIDATION_KEY = "c8a59a8e935f8abb56afb73e0d02b2"
+
+@app.route("/validation-key.txt")
+def validation_key():
+    return Response(VALIDATION_KEY, mimetype="text/plain")
+
 @app.route("/")
 def home():
     return "TokenAnalyzer Live"
 
-@app.route("/validation-key.txt")
-def validate():
-    return Response("c8a59a8e935f8abb56afb73e0d02b2", mimetype="text/plain")
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
